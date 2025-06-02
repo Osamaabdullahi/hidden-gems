1e0583ef674e8af4a8f0b385b41d80a2b3697fe3
osama abdullahi <98954884+Osamaabdullahi@users.noreply.github.com>
2025-06-02 07:28:53 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,4 @@
-"use client";
 import useFetch from "./hooks/UseFetchData";
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import CoolPlaces from "../components/Places";
 import Safari from "../components/features/Safari";
@@ -11,11 +9,6 @@ export default function Home() {
   const fadeInUp = {
     hidden: { opacity: 0, y: 30 },
     visible: { opacity: 1, y: 0 },
-  };
-
-  const fadeIn = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
   };
 
   const { data, error, loading } = useFetch(
@@ -34,7 +27,6 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800 ">
-      <Navbar />
       <HeroSection />
       <CoolPlaces />
       <Safari
